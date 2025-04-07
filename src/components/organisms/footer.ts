685fdeafadc27306958fcc0f99fa082417f32397
gajeev <gajeev@gmail.com>
2025-04-07 08:34:12 -0400

    updates footer call link

--- src/components/organisms/footer.ts
+++ src/components/organisms/footer.ts
@@ -44,9 +44,10 @@ const Footer = () =>
         h(
           Link,
           {
-            href: 'tel:1-647-749-0047',
+            href: 'https://calendly.com/rajahlehal/kickoff',
+            target: '_blank',
           },
-          'Phone: 1-647-749-0047',
+          'Book a call here',
         ),
       ),
     ),
